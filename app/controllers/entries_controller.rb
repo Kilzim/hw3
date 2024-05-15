4440eb9db6e04a8a5a35f728417ad0712070d0bb
Kilian Zimmermann <kilian.zimmermann@kellogg.northwestern.edu>
2024-05-15 18:32:59 +0000

    05/15

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,6 +6,12 @@ class EntriesController < ApplicationController
     # render entry/index view
   end
 
+  def show
+    @entry = entries.find_by({"id" => params["id"]})
+    @place = places.find_by({"id" => @entry["place_id"]})
+  end
+
+
   def new
     # render entry/new view with new Post form
   end
@@ -16,10 +22,11 @@ class EntriesController < ApplicationController
     # assign user-entered form data to Post's columns
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
+    @entry["place_id"] = params["place_id"]
     # save Entry row
     @entry.save
     # redirect user
-    redirect_to "/entries"
+    redirect_to "/places"
   end
 
 end
